d243d2532f66cbd784416ebad831188747a352b5
Utkarsh <mahai.utkarsh@gmail.com>
2021-09-30 17:55:56 -0700

    problem 5 completed. problem 1 issues being resolved

--- Assignment 1/src/edu/northeastern/utkarsh/Problem5.java
+++ Assignment 1/src/edu/northeastern/utkarsh/Problem5.java
@@ -1,9 +1,41 @@
 package edu.northeastern.utkarsh;
 
 public class Problem5 {
-    public static void main(String args[]){
-
+    public static void main(String args[]) {
+        String str = "Aa12s1d3";
+        System.out.print(stringInspection(str));
     }
 
+    public static boolean stringInspection(String str){
+        boolean itIsADigit = false;
+        boolean itIsAnAlphabet = false;
 
+        char characterAtIndex = str.charAt(0);
+
+        if(Character.isDigit(characterAtIndex)){
+            itIsADigit = true;
+        } else {
+            itIsAnAlphabet = true;
+        }
+
+        for(int val = 1; val < str.length(); val++){
+            char checkCharacter = str.charAt(val);
+            if(itIsADigit){
+                if(Character.isDigit(checkCharacter)){
+                    return false;
+                } else{
+                    itIsAnAlphabet = true;
+                    itIsADigit = false;
+                }
+            } else if(itIsAnAlphabet){
+                if(Character.isLetter(checkCharacter)){
+                    return false;
+                } else {
+                    itIsAnAlphabet = false;
+                    itIsADigit = true;
+                }
+            }
+        }
+        return true;
+    }
 }
