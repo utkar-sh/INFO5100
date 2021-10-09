47da000ea4193c9ed78178df55aa49268c823c9e
Utkarsh <mahai.utkarsh@gmail.com>
2021-10-09 12:31:13 -0700

    Solutions to problems 3 & 10 added

--- Assignment 2/src/edu/northeastern/utkarsh/Problem10.java
+++ Assignment 2/src/edu/northeastern/utkarsh/Problem10.java
@@ -1,2 +1,26 @@
-package edu.northeastern.utkarsh;public class Problem10 {
+package edu.northeastern.utkarsh;
+
+//Time Complexity: O(n*n)
+//Space Complexity: O(1)
+public class Problem10 {
+    public static void main(String args[]){
+
+    }
+
+    public int compress(char[] chars) {
+        int indexAns = 0, index = 0;
+        while(index < chars.length){
+            char currentChar = chars[index];
+            int count = 0;
+            while(index < chars.length && chars[index] == currentChar){
+                index++;
+                count++;
+            }
+            chars[indexAns++] = currentChar;
+            if(count != 1)
+                for(char c : Integer.toString(count).toCharArray())
+                    chars[indexAns++] = c;
+        }
+        return indexAns;
+    }
 }
--- Assignment 2/src/edu/northeastern/utkarsh/Problem3.java
+++ Assignment 2/src/edu/northeastern/utkarsh/Problem3.java
@@ -1,2 +1,23 @@
-package edu.northeastern.utkarsh;public class Problem3 {
+package edu.northeastern.utkarsh;
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+public class Problem3 {
+    public static void main(String args[]){
+
+    }
+
+    public void moveZeroes(int[] nums){
+        int k = 0;
+
+        for (int val : nums){
+            if (val != 0) {
+                nums[k++] = val;
+            }
+        }
+
+        for (int val = k; val < nums.length; val++){
+            nums[val] = 0;
+        }
+    }
 }
